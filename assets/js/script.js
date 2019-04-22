c127b5819da5e243d43a1d2a9ab410ea8316fe77
Yacouba Baby <yacoubababy59@gmail.com>
2019-04-22 02:16:34 +0000

    add script.js and mofied path

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+      // Get the modal id01
+      var modal = document.getElementById('id01');
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+
+    // Get the modal id02
+    var modale = document.getElementById('id02');
+
+    // When the user clicks anywhere outside of the modale, close it
+    window.onclick = function(event) {
+      if (event.target == modale) {
+        modale.style.display = "none";
+      }
+    }
+
+      // Get the modal id03
+
+    var moda = document.getElementById('id03');
+
+    // When the user clicks anywhere outside of the modale, close it
+    window.onclick = function(event) {
+      if (event.target == modale) {
+        moda.style.display = "none";
+      }
+    }
